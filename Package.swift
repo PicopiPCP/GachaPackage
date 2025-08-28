bc45fa6faa914e7a51ca55cd07dbf063ff130e8c
BuildTools <unconfigured@null.spigotmc.org>
2025-08-28 20:29:09 +0900

    iroiro shusei

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "Gacha",
+    defaultLocalization: "ja",
     platforms: [
         .iOS(.v15)  // iPadのPlaygroundsが対応している最低バージョンに合わせて
     ],
-    defaultLocalization: "ja",
     products: [
         .library(
             name: "Gacha",
